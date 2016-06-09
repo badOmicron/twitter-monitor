418ef3f70b0af1c52559146857e118c0776b9571
ORLANDO RAMOS <bhit.omicron@gmail.com>
2016-06-08 23:50:00 -0500

    FALTABAN

---
+++ src/main/java/mx/bhit/omicron/app/restful/controller/Trigger.java
@@ -0,0 +1,78 @@
+/**
+ * Trigger.java
+ * Fecha de creación: 08/06/2016, 12:21:05
+ *
+ * Copyright (c) 2016 Instituto Federal Electoral. Dirección
+ * Ejecutiva del Registro Federal de Electores.
+ * Periférico Sur 239, México, D.F., C.P. 01010.
+ * Todos los derechos reservados.
+ *
+ * Este software es información confidencial, propiedad del
+ * Instituto Federal Electoral. Esta información confidencial
+ * no deberá ser divulgada y solo se podrá utilizar de acuerdo
+ * a los términos que determine el propio Instituto.
+ */
+
+package mx.bhit.omicron.app.restful.controller;
+
+import java.util.Timer;
+import java.util.concurrent.Callable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mx.bhit.omicron.app.restful.task.TwitterTask;
+import mx.bhit.omicron.app.restful.utils.TwitterUtil;
+
+/**
+ * TODO [Agregar documentación de la clase]
+ * @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+ * @version 1.0
+ * @since SIIRFE 6.0
+ */
+public class Trigger {
+    private static final Logger logger = LoggerFactory.getLogger(Trigger.class);
+
+    private long timeSlot;
+    private TwitterTask twitterTask;
+    private static Timer timer;
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     */
+    public Trigger() {
+        twitterTask = new TwitterTask();
+    }
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     * @param timeSlot
+     */
+    public void addConfiguration(String timeSlot) {
+        this.timeSlot = Long.parseLong(timeSlot);
+        this.timeSlot = TwitterUtil.miliToSec(this.timeSlot);
+        System.out.println("Segundos " + timeSlot);
+    }
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     */
+    public static void stopMonitoreoTopicos() {
+        timer.cancel();
+    }
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     * @param thread
+     */
+    public void execute(Callable<?> thread) {
+        timer = new Timer();
+        twitterTask.addThread(thread);
+        timer.schedule(twitterTask, 0, this.timeSlot);
+    }
+
+}
---
+++ src/main/java/mx/bhit/omicron/app/restful/task/TwitterTask.java
@@ -0,0 +1,107 @@
+/**
+ * TwitterTask.java
+ * Fecha de creación: 08/06/2016, 13:45:33
+ *
+ * Copyright (c) 2016 Instituto Federal Electoral. Dirección
+ * Ejecutiva del Registro Federal de Electores.
+ * Periférico Sur 239, México, D.F., C.P. 01010.
+ * Todos los derechos reservados.
+ *
+ * Este software es información confidencial, propiedad del
+ * Instituto Federal Electoral. Esta información confidencial
+ * no deberá ser divulgada y solo se podrá utilizar de acuerdo
+ * a los términos que determine el propio Instituto.
+ */
+
+package mx.bhit.omicron.app.restful.task;
+
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import mx.bhit.omicron.app.restful.controller.Trigger;
+import mx.bhit.omicron.app.restful.model.SocialNetworksMonitoredData;
+
+/**
+ * TODO [Agregar documentación de la clase]
+ * @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+ * @version 1.0
+ * @since SIIRFE 6.0
+ */
+public class TwitterTask extends TimerTask {
+    public static ExecutorService executor;
+
+    // private TwitterMonitorThread twitterMonitorThread;
+    private Callable<?> thread;
+    private ObjectMapper mapper;
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     */
+    public TwitterTask() {
+        // TODO [codificar el cuerpo del método]
+        executor = Executors.newCachedThreadPool();
+        mapper = new ObjectMapper();
+    }
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     * @param thread
+     */
+    public void addThread(Callable<?> thread) {
+        this.thread = thread;
+    }
+
+    /* La documentación de este método se encuentra en la clase o interface que
+     * lo declara (non-Javadoc)
+     * @see java.util.TimerTask#run()
+     */
+    @Override
+    public void run() {
+        // TODO [codificar el cuerpo del método]
+        System.out.println("Ejecutando tarea Twitter Monitor");
+        if (thread != null) {
+
+            if (!executor.isShutdown()) {
+                SocialNetworksMonitoredData result;
+                Future<Object> resultObject = (Future<Object>) executor.submit(thread);
+
+                try {
+                    result = (SocialNetworksMonitoredData) resultObject.get();
+                    try {
+                        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result));
+                    } catch (JsonProcessingException e) {
+                        // TODO [Manejar la excepcion de forma correcta]
+                        System.out.println("Error al imprimir Objeto JSON");
+                        e.printStackTrace();
+                        stop();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("No Thread set.");
+        }
+    }
+
+    /**
+     * TODO [Agregar documentacion al método]
+     * @author @author Orlando Adrián Ramos Galván (orlando.ramos@ine.mx, orlandoa.ramos@outlook.com)
+     */
+    public static void stop() {
+        System.out.println("Apagando hilo");
+        Trigger.stopMonitoreoTopicos();
+    }
+
+}
